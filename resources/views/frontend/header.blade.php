50cdfa13b17094e951e3bc02a133ca8fabc56f59
UserFaizan <fazzy093@gmail.com>
2022-12-01 09:53:42 -0800

    new project

---
+++ resources/views/frontend/header.blade.php
@@ -0,0 +1,343 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- slick -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css" integrity="sha512-wR4oNhLBHf7smjy0K4oqzdWumd+r5/+6QO/vDda76MW5iug4PT7v86FoEkySIJft3XA0Ae6axhIvHrqwm793Nw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css" integrity="sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Breeders & Adopters | Home</title>
+  </head>
+  <body>
+    <div class="header">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-6 m-auto">
+            <div class="drop">
+              <div class="dropdown">
+                <button class="dropbtn "><span class="dark-blue-text"><i
+                  class="fa fa-bars fa-1x"></i></span></button>
+                  <div class="dropdown-content">
+                    <a href="#">Ethics</a>
+                    <a href="about.html">About Us</a>
+                    <a href="contact.html">Contact US</a>
+                    <a href="{{route('login')}}">Login</a>
+                    <a href="register.html">Register</a>
+                  </div>
+                </div> 
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="social-btns">
+                <i class="fa fa-facebook-official"></i>
+                <i class="fa fa-linkedin-square"></i>
+              </div>
+              <div class="btn-grp">
+                <button class="btn btn-default"><a href="login.html"><span class="pr-3"><i class="fa fa-sign-in pr-3"></i>Sign In</span></a> | <a href="register.html"><span class="pl-3 pr-3"><i class="fa fa-sign-out  pr-3"></i>Sign Up</span></a></button>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    <a href="#"><h1>Breeders <span style="color: #FF5C00;">&</span> Adopters</h1></a>
+    <h4 class="mb-3">FOVEREVER FINDER</h4> 
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 m-auto">
+          <div class="input-group form-index-page">
+            <input type="text" class="form-control search-top" placeholder="Find you forever family member">
+            <div class="input-group-append">
+              <button class="btn btn-secondary btn-search" type="button">
+                <i class="fa fa-search"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="content">
+        <div class="row mt-5">
+          <div class="col-md-9">
+            <h3 class="breed-heading">A Commonity For Animal Lover’s</h3>
+            <div class="row">
+              <div class="col-md-4">
+                <a href="categories-list.html">
+                  <div class="card pet-card">
+                    <h5 class="card-title">Canines</h5>
+                    <img src="images/dog.png" class="card-img-top" alt="dog">
+                  </div>
+                </div>
+              </a>
+              <div class="col-md-4">
+                <a href="categories-list.html">
+                  <div class="card pet-card">
+                    <h5 class="card-title">felines</h5>
+                    <img src="images/mano.png" class="card-img-top" alt="dog">
+                  </div>
+                </div>
+              </a>
+              <div class="col-md-4">
+                <a href="categories-list.html">
+                  <div class="card pet-card">
+                    <h5 class="card-title">Small Animals</h5>
+                    <img src="images/bandar.png" class="card-img-top" alt="dog">
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div class="row mt-4">
+              <div class="col-md-4">
+                <a href="categories-list.html">
+                  <div class="card pet-card">
+                    <h5 class="card-title">Birds</h5>
+                    <img src="images/mitho.png" class="card-img-top" alt="dog">
+                  </div>
+                </div>
+              </a>
+              <div class="col-md-4">
+                <a href="categories-list.html">
+                  <div class="card pet-card">
+                    <h5 class="card-title">Aquarium</h5>
+                    <img src="images/fish.png" class="card-img-top" alt="dog">
+                  </div>
+                </div>
+              </a>
+              <div class="col-md-4">
+                <a href="categories-list.html">
+                  <div class="card pet-card">
+                    <h5 class="card-title">Reptiles + Amphibians</h5>
+                    <img src="images/raptile.png" class="card-img-top" alt="dog">
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div class="row">
+              <div class="btn-main-page">
+               <a href="categories-list.html"> <button class="btn btn-default btn-vm">View more</button></a>
+              </div>
+            </div>
+            <h3 class="breed-heading">Stores</h3>
+            <div class="row">
+              <div class="col-md-4">
+                <a href="categories-list.html">
+                  <div class="card pet-card">
+                    <h5 class="card-title">Mountain State Feeder Breeders</h5>
+                    <img src="images/dog.png" class="card-img-top" alt="dog">
+                  </div>
+                </div>
+              </a>
+              <div class="col-md-4">
+                <a href="categories-list.html">
+                  <div class="card pet-card">
+                    <h5 class="card-title">CV Exotics inc</h5>
+                    <img src="images/mano.png" class="card-img-top" alt="dog">
+                  </div>
+                </div>
+              </a>
+              <div class="col-md-4">
+                <a href="categories-list.html">
+                  <div class="card pet-card">
+                    <h5 class="card-title">Canadian Regius</h5>
+                    <img src="images/bandar.png" class="card-img-top" alt="dog">
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div class="row">
+              <div class="btn-main-page">
+                 <a href="categories-list.html"><button class="btn btn-default btn-vm">View more</button></a>
+              </div>
+            </div>
+            <h3 class="breed-heading-2">Customer Support</h3>
+            <h4>Client Section</h4>
+            <div class="row mt-4 mb-4">
+              <div class="col-md-2">
+                <div class="img-clients">
+                  <img src="images/img-client.png">
+                </div>
+              </div>
+              <div class="col-md-2">
+                <div class="img-clients">
+                  <img src="images/img-client.png">
+                </div>
+              </div>
+              <div class="col-md-2">
+                <div class="img-clients">
+                  <img src="images/img-client.png">
+                </div>
+              </div>
+              <div class="col-md-2">
+                <div class="img-clients">
+                  <img src="images/img-client.png">
+                </div>
+              </div>
+              <div class="col-md-2">
+                <div class="img-clients">
+                  <img src="images/img-client.png">
+                </div>
+              </div>
+              <div class="col-md-2">
+                <div class="img-clients">
+                  <img src="images/img-client.png">
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="btn-main-page">
+                 <a href="categories-list.html"><button class="btn btn-default btn-vm">View more</button></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="right-sidebar">
+              <h5>News</h5>
+              <ul class="list-group" style="border-radius: 0px;">
+                <li class="list-group-item border-top-0 border-left-0 border-right-0">Oct 12) Genetic Testing Now Available!</li>
+                <li class="list-group-item border-top-0 border-left-0 border-right-0">Oct 12) Genetic Testing Now Available!</li>
+                <li class="list-group-item border-top-0 border-left-0 border-right-0">Oct 12) Genetic Testing Now Available!</li>
+                <li class="list-group-item border-top-0 border-left-0 border-right-0">Oct 12) Genetic Testing Now Available!</li>
+                <li class="list-group-item border-top-0 border-left-0 border-right-0">Oct 12) Genetic Testing Now Available!</li>
+              </ul>
+              <div class="view-all">
+                <button class="btn btn-default btn-view"> view all</button>
+              </div>
+            </div>
+            <div class="right-sidebar mt-5">
+              <h5>News</h5>
+              <ul class="list-group" style="border-radius: 0px;">
+                <li class="list-group-item border-top-0 border-left-0 border-right-0">Oct 12) Genetic Testing Now Available!</li>
+                <li class="list-group-item border-top-0 border-left-0 border-right-0">Oct 12) Genetic Testing Now Available!</li>
+                <li class="list-group-item border-top-0 border-left-0 border-right-0">Oct 12) Genetic Testing Now Available!</li>
+                <li class="list-group-item border-top-0 border-left-0 border-right-0">Oct 12) Genetic Testing Now Available!</li>
+                <li class="list-group-item border-top-0 border-left-0 border-right-0">Oct 12) Genetic Testing Now Available!</li>
+              </ul>
+              <div class="view-all">
+                <button class="btn btn-default btn-view"> view all</button>
+              </div>
+            </div>
+            <div class="slider-right">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-12 pt-5">
+                    <div class="background p-3">
+                      <div style="height: 150px;"></div>
+                      <h4>Testimonails</h4>
+                      <h5>Client are saying</h5>
+                      <div class="row responsive mt-4" style="box-shadow: 10px 10px 10px rgba(0, 127, 128, 0.26);">
+                        <div class="card cardbbg  mx-auto " style="width;100%;">
+                          <div class="card-body p-2">
+                            <!-- <h5 class="card-title">Card title</h5> -->
+                            <p class="card-text ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, voluptatem voluptatibus dolores consequatur </p>
+                            <img class="round" src="images/testi-img.png" alt="testimonial-image">
+                            <h6>Jared kornoson</h6>
+                          </div>
+                        </div>
+                        <div class="card cardbbg mx-auto " style="width: 100%;">
+                          <div class="card-body p-2">
+                            <!-- <h5 class="card-title">Card title</h5> -->
+                            <p class="card-text ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, voluptatem voluptatibus dolores consequatur</p>
+                            <img class="round" src="images/testi-img.png" alt="testimonial-image">
+                            <h6>Jared kornoson</h6>
+                          </div>
+                        </div>
+                        <div class="card cardbbg mx-auto " style="width: 100%;">
+                          <div class="card-body p-2">
+                            <!-- <h5 class="card-title">Card title</h5> -->
+                            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, voluptatem voluptatibus dolores consequatur </p>
+                            <img class="round" src="images/testi-img.png" alt="testimonial-image">
+                            <h6>Jared kornoson</h6>
+                          </div>
+                        </div>
+                        <div class="card cardbbg mx-auto " style="width: 100%;">
+                          <div class="card-body p-2">
+                            <!-- <h5 class="card-title">Card title</h5> -->
+                            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, voluptatem voluptatibus dolores consequatur </p>
+                            <img class="round" src="images/testi-img.png" alt="testimonial-image">
+                            <h6>Jared kornoson</h6>
+                          </div>
+                        </div>
+                        <div class="card cardbbg mx-auto " style="width: 100%;">
+                          <div class="card-body p-2">
+                            <!-- <h5 class="card-title">Card title</h5> -->
+                            <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, voluptatem voluptatibus dolores consequatur .</p>
+                            <img class="round" src="images/testi-img.png" alt="testimonial-image">
+                            <h6>Jared kornoson</h6>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="footer-breed">
+        <h2>Copyright © 2022</h2>
+      </div>
+    </footer>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.js" integrity="sha512-WNZwVebQjhSxEzwbettGuQgWxbpYdoLf7mH+25A7sfQbbxKeS5SQ9QBf97zOY4nOlwtksgDA/czSTmfj4DUEiQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+    <script>
+     $('.responsive').slick({
+       dots: true,
+       infinite: false,
+       arrows: false,
+       speed: 300,
+       slidesToShow: 1,
+       slidesToScroll: 1,
+       responsive: [
+       {
+         breakpoint: 1024,
+         settings: {
+           slidesToShow: 1,
+           slidesToScroll: 1,
+           infinite: true,
+           dots: true
+         }
+       },
+       {
+         breakpoint: 600,
+         settings: {
+           slidesToShow: 1,
+           slidesToScroll: 1
+         }
+       },
+       {
+         breakpoint: 480,
+         settings: {
+           slidesToShow: 1,
+           slidesToScroll: 1
+         }
+       }
+        ]
+         });
+    </script>
+                   
+  </body>
+</html>
