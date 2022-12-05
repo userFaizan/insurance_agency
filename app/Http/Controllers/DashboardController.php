a30268474f327804ddc7580d54cb6b3e99b6d269
UserFaizan <fazzy093@gmail.com>
2022-12-05 05:30:45 -0800

    todays work

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,20 +8,20 @@ use Auth;
 class DashboardController extends Controller
 {
    
-//     public function redirection()
-//     {
-//         if(Auth::user()->hasRole('employee')){
-//              return view('employee_dashboard');
-//         }elseif(Auth::user()->hasRole('admin')){
-//          return view('dashboard');
-//     }elseif(Auth::user()->hasRole('in_house_agent')){
-//         return view('in_house_agent_dashboard');
-//    }elseif(Auth::user()->hasRole('downline_agent')){
-//     return view('downline_agent_dashboard');
-// }elseif(Auth::user()->hasRole('referral_agent')){
-//     return view('referral_agent_dashboard');
-// }elseif(Auth::user()->hasRole('customer')){
-//     return view('customer_dashboard');
-// }
-//     }
+    public function redirection()
+    {
+        if(Auth::user()->hasRole('employee')){
+             return view('employee_dashboard');
+        }elseif(Auth::user()->hasRole('admin')){
+         return view('dashboard');
+    }elseif(Auth::user()->hasRole('in_house_agent')){
+        return view('in_house_agent_dashboard');
+   }elseif(Auth::user()->hasRole('downline_agent')){
+    return view('downline_agent_dashboard');
+}elseif(Auth::user()->hasRole('referral_agent')){
+    return view('referral_agent_dashboard');
+}elseif(Auth::user()->hasRole('customer')){
+    return view('customer_dashboard');
+}
+    }
 }
