0bb5e035ff1e515e30e1f766e7a5695af5c7d1c8
Umair Butt <buttjeefull77@gmail.com>
2022-12-12 18:16:29 +0000

    vendor_details

---
+++ resources/views/vendor/vendor_details.blade.php
@@ -0,0 +1,348 @@
+@extends('admin_frontend.master')
+@section('content')
+
+<div class="content-wrapper">
+
+<!-- form start  -->
+
+    <div class="col-12 grid-margin">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="card-title">Vendor Details</h4>
+
+          <form class="form-sample">
+
+            <p class="card-description"> Vendor </p>
+<!-- row -->
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">DISPLAY NAME</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">name</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Vendor Name</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">name</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Tax ID # </label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">id</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Tel  </label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">tel</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Fax  </label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">fax</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Email </label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">email</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Address   </label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">address</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Website  </label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">url</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Notes   </label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">notes</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                 
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<p class="card-description"> Files</p>
+
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Type of Vendor</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted"></p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Account #</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">account</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Account Holder Name</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">account holder</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Person In Charge</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">person</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Due Date</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">date</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Amount Constant</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">constant</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<div class="row">
+<div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Billing Terms</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">terms</p>
+                  </div>
+                </div>
+              </div>
+              
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Payment Method</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">method</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<p class="card-description"> Log In(s) Info</p>
+
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Website</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">url</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">User Name</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">name</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Password</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">pass</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">PIN</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">pin</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Notes</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">notes</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                </div>
+              </div>
+            </div>
+<!-- row -->
+
+<p class="card-description"> Point of Contact  </p>
+
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Name</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">name</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Title</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">title</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Address</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">address</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Tel</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">tel</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Fax</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">fax</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Email</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">email</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+<!-- row -->
+<div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Website</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">website</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label class="col-sm-3 col-form-label">Notes:</label>
+                  <div class="col-sm-9">
+                  <p class="col-form-label text-muted">message</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+
+<!-- row -->
+
+             </form>
+        </div>
+      </div>
+    </div>
+    <!-- form end -->
+  </div>
+ 
+  @endsection
